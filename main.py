599771854f60053437ab4d244aca731bd62442a6
Irina Maksimova <77109276+IrinaMaksimova@users.noreply.github.com>
2022-01-17 12:26:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+teksts = input("Ievadiet skaitli: ")
+def countNumbers (teksts): 
+  summa = 0
+  for simbols in teksts:
+   summa = summa + int(simbols)
+  print(summa)
+  return summa 
+countNumbers (teksts)
